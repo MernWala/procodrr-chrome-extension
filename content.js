97167655b6849e9fcdf6d904d20b3719d2fee508
Anurag Singh <61440053+anuragsinghbam@users.noreply.github.com>
2025-01-08 14:38:16 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,232 @@
-(()=>{let e=!1;function t(){document.removeEventListener("click",t),document.removeEventListener("keydown",t),document.removeEventListener("touchstart",t)}document.addEventListener("click",t),document.addEventListener("keydown",t),document.addEventListener("touchstart",t);class a{constructor(e){this.video=e}Space(e){let t=this.video;"textarea"===e.target.type||"input"===e.target.type||e.target.isContentEditable||(e.preventDefault(),t.paused?t.play():t.pause())}ArrowLeft(){document.querySelector(".bmpui-ui-rewindbutton").click()}ArrowRight(){document.querySelector(".bmpui-ui-forwardbutton").click()}ArrowUp(e){let t=this.video;if(e.preventDefault(),t.volume<1){let e=(t.volume+.1).toFixed(1);t.volume=e,localStorage.setItem("volume",e),t.muted=!1}}ArrowDown(e){let t=this.video;if(e.preventDefault(),t.volume>0){let e=(t.volume-.1).toFixed(1);t.volume=e,localStorage.setItem("volume",e),t.muted=!1}else t.muted=!0}BracketLeft(){let e=this.video,t=(e.playbackRate-.1).toFixed(1);t>=0&&(e.playbackRate=t,o.innerText=t+"x",localStorage.setItem("playbackSpeed",e.playbackRate),o.classList.contains("hidden")&&(o.classList.remove("hidden"),setTimeout(()=>{o.classList.add("hidden")},1e3)))}BracketRight(){let e=this.video,t=(e.playbackRate+.1).toFixed(1);t<=16&&(e.playbackRate=t,o.innerText=t+"x",localStorage.setItem("playbackSpeed",e.playbackRate),o.classList.contains("hidden")&&(o.classList.remove("hidden"),setTimeout(()=>{o.classList.add("hidden")},1e3)))}KeyD(e){document.body.classList.add("dark")}KeyL(e){document.body.classList.remove("dark")}KeyI(e){document.querySelector(".bmpui-ui-piptogglebutton").click()}KeyF(){document.querySelector(".bmpui-ui-fullscreentogglebutton").click()}KeyM(){let e=this.video;e.muted=!e.muted}}document.createElement("div").classList.add("btn-container"),document.addEventListener("keydown",e=>{let t=document.querySelector("video");if("textarea"===e.target.type||"input"===e.target.type||e.target.isContentEditable||("Space"===e.code&&e.preventDefault(),!t))return;let l=new a(t);l[e.code]?.(e,t)});let l=null,o=null,n=document.getElementById("root");new MutationObserver(()=>{let t=document.querySelector("video");if(null!==l&&t===l)return;l=t;let a=document.querySelector(".menu_sidebar:has(li.ant-menu-item)");document.querySelector("#play");let n=document.querySelector(".bitmovinplayer-container");console.log({playerContainer:n}),n&&((o=document.createElement("span")).className="playback-speed hidden",n.append(o)),a&&!e&&(a.addEventListener("click",e=>{if(e.target.closest("li.ant-menu-item")){let e=setInterval(()=>{let t=document.querySelector("#play");t&&t?.click();let a=document.querySelector("video"),l=document.querySelector(".show-v-a-a-images")?.lastChild;(a&&!a.paused||l&&l.className.includes("scaffold"))&&clearInterval(e)},100)}}),e=!0),l&&(l.addEventListener("play",function(){!function(e){let t=localStorage.getItem("playbackSpeed"),a=localStorage.getItem("volume");t&&(e.playbackRate=t,o.innerText=t+"x"),a&&(e.volume=a)}(l)}),l.addEventListener("volumechange",e=>{localStorage.setItem("volume",l.volume)}),l.addEventListener("ratechange",()=>{localStorage.setItem("playbackSpeed",l.playbackRate)}))}).observe(n,{childList:!0,subtree:!0})})();
+let userInteracted = false;
+let sideBarFound = false;
+
+function onUserInteraction() {
+  userInteracted = true;
+  // Remove the event listeners after the first interaction
+  document.removeEventListener("click", onUserInteraction);
+  document.removeEventListener("keydown", onUserInteraction);
+  document.removeEventListener("touchstart", onUserInteraction);
+}
+
+// Add event listeners for user interaction
+document.addEventListener("click", onUserInteraction);
+document.addEventListener("keydown", onUserInteraction);
+document.addEventListener("touchstart", onUserInteraction);
+
+class Shortcuts {
+  constructor(video) {
+    this.video = video;
+  }
+  Space(e) {
+    const video = this.video;
+    const targetIsInput =
+      e.target.type === "textarea" ||
+      e.target.type === "input" ||
+      e.target.isContentEditable;
+    if (targetIsInput) return;
+
+    e.preventDefault();
+
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+  ArrowLeft() {
+    document.querySelector(".bmpui-ui-rewindbutton").click();
+  }
+  ArrowRight() {
+    document.querySelector(".bmpui-ui-forwardbutton").click();
+  }
+  ArrowUp(e) {
+    const video = this.video;
+    e.preventDefault();
+    if (video.volume < 1) {
+      const newVolume = (video.volume + 0.1).toFixed(1);
+
+      video.volume = newVolume;
+      localStorage.setItem("volume", newVolume);
+      video.muted = false;
+    }
+  }
+  ArrowDown(e) {
+    const video = this.video;
+    e.preventDefault();
+    if (video.volume > 0) {
+      const newVolume = (video.volume - 0.1).toFixed(1);
+
+      video.volume = newVolume;
+      localStorage.setItem("volume", newVolume);
+      video.muted = false;
+    } else {
+      video.muted = true;
+    }
+  }
+  BracketLeft() {
+    const video = this.video;
+    const newPlaybackRate = (video.playbackRate - 0.1).toFixed(1);
+    if (newPlaybackRate >= 0) {
+      video.playbackRate = newPlaybackRate;
+      speedContainer.innerText = newPlaybackRate + "x";
+      localStorage.setItem("playbackSpeed", video.playbackRate);
+      if (speedContainer.classList.contains("hidden")) {
+        speedContainer.classList.remove("hidden");
+        setTimeout(() => {
+          speedContainer.classList.add("hidden");
+        }, 1000);
+      }
+    }
+  }
+  BracketRight() {
+    const video = this.video;
+    const newPlaybackRate = (video.playbackRate + 0.1).toFixed(1);
+    if (newPlaybackRate <= 16) {
+      video.playbackRate = newPlaybackRate;
+      speedContainer.innerText = newPlaybackRate + "x";
+      localStorage.setItem("playbackSpeed", video.playbackRate);
+      if (speedContainer.classList.contains("hidden")) {
+        speedContainer.classList.remove("hidden");
+        setTimeout(() => {
+          speedContainer.classList.add("hidden");
+        }, 1000);
+      }
+    }
+  }
+  KeyD(e) {
+    document.body.classList.add("dark");
+  }
+  KeyL(e) {
+    document.body.classList.remove("dark");
+  }
+  KeyI(e) {
+    document.querySelector(".bmpui-ui-piptogglebutton").click();
+  }
+  KeyF() {
+    document.querySelector(".bmpui-ui-fullscreentogglebutton").click();
+  }
+  KeyM() {
+    const video = this.video;
+    video.muted = !video.muted;
+  }
+}
+
+const themeToggleBtn = document.createElement("div");
+themeToggleBtn.classList.add("btn-container");
+
+function previousValues(video) {
+  const playbackRate = localStorage.getItem("playbackSpeed");
+  const volume = localStorage.getItem("volume");
+  if (playbackRate) {
+    video.playbackRate = playbackRate;
+    speedContainer.innerText = playbackRate + "x";
+  }
+  if (volume) video.volume = volume;
+}
+
+document.addEventListener("keydown", (e) => {
+  const video = document.querySelector("video");
+  const targetIsInput =
+    e.target.type === "textarea" ||
+    e.target.type === "input" ||
+    e.target.isContentEditable;
+  if (targetIsInput) return;
+  if (e.code === "Space") {
+    e.preventDefault();
+  }
+  if (!video) return;
+  const shortcuts = new Shortcuts(video);
+  shortcuts[e.code]?.(e, video);
+});
+
+let shortcutsAdded = false;
+
+let video = null;
+let speedContainer = null;
+const targetNode = document.getElementById("root");
+const observer = new MutationObserver(() => {
+  const newVideo = document.querySelector("video");
+  if (video !== null && newVideo === video) return;
+  video = newVideo;
+
+  const sideBar = document.querySelector(".menu_sidebar:has(li.ant-menu-item)");
+  const playbutton = document.querySelector("#play");
+  const playerContainer = document.querySelector(".bitmovinplayer-container");
+  console.log({ playerContainer });
+  if (playerContainer) {
+    const newSpeedContainer = document.createElement("span");
+    speedContainer = newSpeedContainer;
+    speedContainer.className = "playback-speed hidden";
+    playerContainer.append(speedContainer);
+  }
+  // if (
+  //   playbutton &&
+  //   userInteracted &&
+  //   location.href !== "https://app.procodrr.com/web/activity"
+  // ) {
+  //   playbutton?.click();
+  // }
+  if (sideBar && !sideBarFound) {
+    sideBar.addEventListener("click", (e) => {
+      if (e.target.closest("li.ant-menu-item")) {
+        const intervalId = setInterval(() => {
+          const playbutton = document.querySelector("#play");
+
+          if (playbutton) {
+            playbutton?.click();
+          }
+          const video = document.querySelector("video");
+          const player =
+            document.querySelector(".show-v-a-a-images")?.lastChild;
+          if (
+            (video && !video.paused) ||
+            (player && player.className.includes("scaffold"))
+          ) {
+            clearInterval(intervalId);
+          }
+        }, 100);
+      }
+    });
+    sideBarFound = true;
+  }
+  if (video) {
+    video.addEventListener("play", function () {
+      previousValues(video);
+    });
+    video.addEventListener("volumechange", (e) => {
+      localStorage.setItem("volume", video.volume);
+    });
+    video.addEventListener("ratechange", () => {
+      localStorage.setItem("playbackSpeed", video.playbackRate);
+    });
+  }
+});
+
+observer.observe(targetNode, {
+  childList: true,
+  subtree: true,
+});
+
+function toggleFullScreen() {
+  const videoElement = document.querySelector("video");
+
+  if (document.fullscreenElement) {
+    // Exit fullscreen mode
+    document.exitFullscreen();
+  } else {
+    // Enter fullscreen mode
+    if (videoElement.requestFullscreen) {
+      videoElement.requestFullscreen();
+    } else if (videoElement.mozRequestFullScreen) {
+      // Firefox
+      videoElement.mozRequestFullScreen();
+    } else if (videoElement.webkitRequestFullscreen) {
+      // Chrome, Safari and Opera
+      videoElement.webkitRequestFullscreen();
+    } else if (videoElement.msRequestFullscreen) {
+      // IE/Edge
+      videoElement.msRequestFullscreen();
+    }
+  }
+}
